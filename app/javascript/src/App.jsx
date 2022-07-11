3689ab81efce498992b1cb27564f8c4bd11aad00
Jenipha Rachel <jenipharachel@gmail.com>
2022-07-11 13:09:44 +0530

    Sign up form UI

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -4,8 +4,8 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import { setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
-
-import Dashboard from "./components/Dashboard";
+import Signup from "components/Authentication/Signup";
+import Dashboard from "components/Dashboard";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -23,6 +23,7 @@ const App = () => {
     <Router>
       <Switch>
         <Route exact path="/" component={Dashboard} />
+        <Route exact path="/signup" component={Signup} />
       </Switch>
     </Router>
   );
